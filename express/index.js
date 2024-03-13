ee78fb4ffecaa5fc27668ffc90f822829c925021
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-03-13 22:06:57 +0530

    authentication

--- express/index.js
+++ express/index.js
@@ -1,112 +1,121 @@
 import express from "express";
 import path from "path";
-import dotenv from 'dotenv'
-import mongoose, { Schema, mongo } from "mongoose";
+import { connectDB, User } from "./db/index.js";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+import bcrypt from 'bcrypt'
 
-
-
-const app = express()
-
-dotenv.config({
-    path:'./.env'
-})
-
+const app = express();
 // database
- 
-await mongoose.connect(process.env.MONGO_URL).then(()=> (
-    console.log("database connected")
-)).catch((error) => console.log("error", error))
-
-const messageSchema = new mongoose.Schema({
-    name: {
-       type: String,
-       required: true
-    },
-
-    email: {
-        type: String,
-        required: true
-    }
-
-}, {timestamps:true})
-
-const Message = mongoose.model("Message", messageSchema)
-
-const PORT = process.env.PORT
-
+connectDB();
+const PORT = process.env.PORT;
 
 // setting up view engine
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-// serving html file 
+// serving html file
 // one way it will give error
 // express.static(path.join(path.resolve(), 'public'))
 
-
 // middlewares
-app.use(express.static(path.join(path.resolve(), 'public')))
-app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-   
-    res.render("index.ejs", {name: "Abhishek"})
-    // res.sendFile("index")
+
+// authintication
+const isAuthenticated = async (req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const decoded = jwt.verify(token, "fskfwfnkkfsjkfsk")
+    req.user = await User.findById(decoded._id)
+    // console.log(decoded);
+    next()
+  } else {
+    res.redirect("/login")
+  }
+};
+
+
+app.get("/", isAuthenticated, (req, res) => {
+
+  res.render("logout", { name: req.user.name })
+});
+
+app.get("/login", isAuthenticated, (req, res) => {
+  res.render("login")
 })
 
-app.get('/add', async (req, res) => {
-     await Message.create({name: "abhi2", email:'sample2@gmail.com'})
-     res.send("Data added successfully")
-})
+app.get("/register", (req, res) => {
+  res.render("register")
+});
 
-app.get('/success', (req, res) => {
-    res.render("success")
-})
+// register auth
 
-app.get("/users", (req, res) => {
-    res.json({
-        success:true
+app.post("/register", async (req, res) => {
+
+  const { name, email, password } = req.body
+  const hashPassword = bcrypt.hash(password, 10)
+  let user = await User.findOne({ email })
+  if (user) {
+    res.redirect("/login")
+  }
+
+  else {
+    user = await User.create({
+      name,
+      email,
+      password: hashPassword
     })
+
+    const token = jwt.sign({ _id: user._id }, "fskfwfnkkfsjkfsk",)
+    // console.log(token);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 60 * 1000),
+    });
+    res.redirect("/");
+  }
+});
+
+// login auth
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body
+  let user = await User.findOne({ email })
+  if (!user) {
+    res.redirect("/register")
+  }
+  const isMatch = await bcrypt.compare(password, user.password)
+  if (!isMatch) {
+    res.render("login", { message: "Incorrect Password" })
+  }
+  else {
+    const token = jwt.sign({ _id: user._id }, "fskfwfnkkfsjkfsk",)
+    res.cookie("token", token, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 60 * 1000),
+    });
+    res.redirect("/");
+
+  }
 })
 
-app.post("/contact", async (req, res) => {
-     const {name, email} = req.body
-     await Message.create({name, email})
-     res.redirect('/success')
-})
+// logout auth
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", null, {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.redirect("/register");
+});
+
+
+
 
 
 app.listen(PORT, () => {
-    console.log(`server is running ${PORT}`);
-})
-
-
-
-
-
-
-
-
-
-// app.get('/', (req, res)=> {
-   
-//     //    res methods
-
-//     // res.sendStatus(400)
-//     // res.send("hii")
-//     // res.json({
-//     //     success:true,
-//     //     products: []
-//     // })
-
-//     // res.status(200).json({
-//     //     success:true,
-//     //     product: []
-//     // })
-// })
-
-
- // const location = path.resolve()
-    // console.log(path.join(location, "/index.html"));
-    // res.sendFile(path.join(location, "/index.html"))
-
-    // sending html file 
+  console.log(`server is running ${PORT}`);
+});
